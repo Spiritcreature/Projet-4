d69129bbfe9cd28429a3cd7873a5b4a169dba8f5
Alexis <dizet.alexis@gmail.com>
2018-07-20 15:22:57 +0200

    Mise en place de la vur chapitre

---
+++ view/frontend/chapterView.php
@@ -0,0 +1,30 @@
+<?php $title = 'Un billet pour l\'Alaska !'; ?>
+
+<?php ob_start(); ?>
+
+
+<h1>Les dernières publications.</h1>
+
+<?php
+while ($data = $allPosts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']) ?>
+            <em>le <?= $data['creation_date_fr'] ?></em>
+        </h3>
+        
+        <p>
+            <?= nl2br(substr(htmlspecialchars($data['content']),0,840) . '...') ?>
+            <a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire la suite.</a>
+        </p>
+    </div>
+
+<?php
+}
+$allPosts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
