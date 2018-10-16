f9dcfece3aa7b2caffae0a75fa9baaae466e86e2
Alexis <dizet.alexis@gmail.com>
2018-10-16 11:23:06 +0200

    Modification de Database.php pour que PDO s'initialse qu'une fois si la connexion à la BDD est demandé plusieurs fois alors qu'elle est déjà executée.

--- model/frontend/Database.php
+++ model/frontend/Database.php
@@ -2,12 +2,20 @@
 
 class Database
 {
+	
+	private $pdo;
+	
     protected function dbConnect()
     {
 		try
 		{
-			$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) );
-			return $db;
+			// cette condition permet d'initialiser une seule fois PDO si la connexion est demandée plusieurs fois
+			if($this->pdo === null)
+			{
+				$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) );
+				$this->pdo = $db;
+			}	
+			return $this->pdo;	
 		}
 		catch (Exception $e)
 		{
